088345b9ed20d7497e07ab94b9b9b3de8561005f
Jashany <jsingh12_be22@thapar.edu>
2023-10-10 00:50:34 +0530

    Hello

---
+++ index.js
@@ -0,0 +1,25 @@
+document.querySelector("body").addEventListener("mousemove", eyeball);
+
+function eyeball() {
+    'use strict';
+    var eye = document.querySelectorAll(".eye");
+    eye.forEach(function (eye) {
+        let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2)
+        let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
+        let radian = Math.atan2(event.pageX - x, event.pageY - y);
+        let rot = (radian * (180 / Math.PI) * -1) + 270 +90;
+        eye.style.transform  = "rotate("+ rot +"deg)"
+    })
+}
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(el => {
+  el.addEventListener('blur', e => {
+    if(e.target.value) {
+      e.target.classList.add('dirty');
+    } else {
+      e.target.classList.remove('dirty');
+    }
+  })
+})
